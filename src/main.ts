01102e0f713baaa1eaa9439e6f5d928c6b86291d
SirCypkowskyy <dcyprian.a.gburek@gmail.com>
2025-08-10 16:27:27 +0200

    J: add lint checks with unused function

--- src/main.ts
+++ src/main.ts
@@ -40,6 +40,11 @@ if (db) {
   v1Router.post("/notes", middlewareAuth(handlerNotesCreate));
 }
 
+function unused() {
+  // this function does nothing
+  // and is called nowhere
+}
+
 v1Router.get("/healthz", handlerReadiness);
 
 app.use("/v1", v1Router);
@@ -47,3 +52,4 @@ app.use("/v1", v1Router);
 app.listen(config.api.port, () => {
   console.log(`Server is running on port: ${config.api.port}`);
 });
+
